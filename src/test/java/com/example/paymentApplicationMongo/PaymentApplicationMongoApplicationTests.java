dcb55002f242c6e2e6d3a547ddef8ad04864cb9c
manoejKr <287642@ust.com>
2024-10-21 16:56:42 +0530

    first commit

---
+++ src/test/java/com/example/paymentApplicationMongo/PaymentApplicationMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentApplicationMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
